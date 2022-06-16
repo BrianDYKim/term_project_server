8fe4b3c11ffce97c3a286849b4ec8737a13061c8
BrianDYKim <18k7102dy@naver.com>
2022-06-17 03:19:18 +0900

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 100
+#define MAX_CLNT 255 // 최대 접속 클라이언트 수는 255로 제한
+
+void error_handling(char *msg); // 에러 발생시 에러를 핸들링하는 함수
+
+int main(int argc, char *argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+void error_handling(char *msg) {
+    fputs(msg, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
