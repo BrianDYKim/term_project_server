dc1775b2f651a421f489cb696971c740462f5a38
Ubuntu <ubuntu@ip-10-1-0-251.ap-northeast-2.compute.internal>
2022-06-17 16:17:51 +0000

    pthread 문제 해결

---
+++ build/CMakeFiles/term_project_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/term_project_server.dir/client_list.c.o"
+  "CMakeFiles/term_project_server.dir/main.c.o"
+  "term_project_server"
+  "term_project_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/term_project_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
